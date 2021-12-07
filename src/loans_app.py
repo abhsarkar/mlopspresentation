9bb349f1f52b024b6171a91c1ed9919cba5ec9bd
Abhishek Sarkar <abhisheksarkar@Abhisheks-MacBook-Air.local>
2021-12-07 23:13:01 +0530

    commit message goes here

--- src/loans_app.py
+++ src/loans_app.py
@@ -9,36 +9,66 @@ from gradio import gradio as gr
 #ARTIFACT_NAME = 'auto_loan:v0'
 #artifact = run.use_artifact('abhsarkar/auto_loan/' + ARTIFACT_NAME, type='model')
 #artifact_dir = artifact.download()
+
 x_columns = ['Client_Gender','Client_Occupation','Car_Owned','Client_Education','Active_Loan','Cleint_City_Rating','Workphone_Working',
              'House_Own','Type_Organization','Client_Income_Type','Registration_Days','Child_Count','Credit_Amount']
-cat_features = ['Car_Owned','Bike_Owned','Active_Loan','House_Own',
-                'Mobile_Tag','Homephone_Tag','Workphone_Working','Cleint_City_Rating']
+
+
+cat_features = ['Car_Owned', 'Bike_Owned', 'Active_Loan', 'House_Own',
+                'Mobile_Tag', 'Homephone_Tag', 'Workphone_Working', 'Cleint_City_Rating']
+
 num_features = list(set(x_columns) - set(cat_features))
+def predict_price(clientGender, clientOccupation, carOwn,
+                  clientEdu, activeLoan, clientCity, 
+                  workPhone, ownHouse, orgType, 
+                  incomeType, regDays, children,amntNeed):
 
-def predict_score(ownedcredit, age, genderType, married, vehicleown):
-
-  #  inputs_dict = {'KM_Driven' : float(kmDriven), 
-          
-   # df = pd.DataFrame(inputs_dict, index = [0])
-    #print(df)
-
-    #inputs = {c: df[c].values for c in df.columns}
-    #for c in num_features:
-      #  inputs[c] = inputs[c].astype(np.float32)
-    #for k in inputs:
-       # inputs[k] = inputs[k].reshape((inputs[k].shape[0], 1))            
   
-    #sess = rt.InferenceSession(artifact_dir + '/usedcar_xgboost.onnx')
-    #pred_onx = sess.run(None, inputs)
+    inputs_dict = {'Client_Gender' : float(clientGender), 
+              'Client_Occupation': float(clientOccupation), 
+              'Car_Owned': float(carOwn), 
+              'Client_Education': float(clientEdu), 
+              'Active_Loan': float(activeLoan), 
+              'Cleint_City_Rating': float(clientCity), 
+              'Workphone_Working': float(workPhone), 
+              'House_Own': float(ownHouse), 
+              'Type_Organization': float(orgType), 
+              'Client_Income_Type': float(incomeType), 
+              'Registration_Days': float(regDays), 
+              'Child_Count': float(children),
+              'Credit_Amount': float(amntNeed)}
+    
 
-    predicted_score = float(500)
-    return {f'Expected score {np.round(predicted_price, predicted_score)}' }
+    df = pd.DataFrame(inputs_dict, index = [0])
+    print(df)
+
+    inputs = {c: df[c].values for c in df.columns}
+    for c in num_features:
+        inputs[c] = inputs[c].astype(np.float32)
+    for k in inputs:
+        inputs[k] = inputs[k].reshape((inputs[k].shape[0], 1))            
+  
+    sess = rt.InferenceSession(artifact_dir + '../artifacts/autoloan_xgboost.onnx')
+    pred_onx = sess.run(None, inputs)
+
+    predicted_price = float(pred_onx[0][0,0]) }
 
 
-ownedcredit = gr.inputs.Slider(minimum=10, maximum=500, label="Credit Points")
-age = gr.inputs.Dropdown(list(range(18, 100)), default="1", label="Age of the Credit Taker ((in years): ")
-genderType = gr.inputs.Radio(['Male', 'Female', 'Others;'], default="Female", label="Client Gender")
-married = gr.inputs.Radio(['Single', 'Married'], default="Single", label="Marriage Status")
-vehicleown = gr.inputs.Radio(['4 Wheeler', '2 Wheeler', 'Both','None'], default="None", label="Vehicle Owner Type")
+activeLoan = gr.inputs.Slider(minimum=10, maximum=500, label="activeLoan  Lakhs")
+clientGender = gr.inputs.Radio(['Male', 'Female', 'Others;'], default="Female", label="Client Gender")
+amntNeed = gr.inputs.Radio(['50000', '5000000', '1000000','7500000'], default="0", label="Amount Needed")
 
-#gr.Interface(predict_score, [ownedcredit, age, genderType, married, vehicleown], "text",live=False).launch(debug=True,share=true);
+gr.Interface(predict_score, [clientGender,
+                          activeLoan,
+                          amntNeed,
+                          clientEdu=float(2), 
+                          clientOccupation=float(4),
+                        carOwn=float(8)
+                          clientCity=4, 
+                          workPhone=5, 
+                          ownHouse=1,
+                          orgType=55, 
+                          incomeType=1000.0, 
+                          regDays=185.0, 
+                          children=4
+                          ], "text",live=False).launch(debug=True,share=true);
